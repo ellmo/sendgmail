3e7ca4e88967a500605fdba61932b2049635cab6
ellmo <ellmunzai@gmail.com>
2012-09-19 00:26:41 +0200

    sending attachment

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 
 gem 'tlsmail'
 gem 'parseconfig'
+gem 'pry'
--- sendgmail.rb
+++ sendgmail.rb
@@ -1,6 +1,15 @@
+#!/usr/bin/env ruby
 require 'tlsmail'
 require 'time'
 require 'parseconfig'
+require 'pry'
+
+subject = ARGV[0]
+filename = ARGV[1]
+if filename
+  filecontent = File.read(filename)
+  encodedcontent = [filecontent].pack("m")   # base64
+end
 
 email_config = ParseConfig.new('email.conf').params
 from = email_config['from_address']
@@ -10,10 +19,10 @@ p = email_config['p']
 content = <<EOF
 From: #{from}
 To: #{to}
-subject: TEST from ruby
+subject: #{subject || 'no subject'} 
 Date: #{Time.now.rfc2822}
 
-
+#{encodedcontent}
 EOF
 
 Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)  
